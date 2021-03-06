23f0a5a787a8913862f99bc6d0b3277c0279baaf
Quazar8 <barcode12d@gmail.com>
2021-03-06 23:04:34 +0200

    Linking a js file which will containt the logic to the project

---
+++ src/logic.js
@@ -0,0 +1,4 @@
+const original = document.getElementById('original')
+const originalEncrypted = document.getElementById('original-encrypted')
+const scrambled = document.getElementById('scrambled')
+const decrypted = document.getElementById('decrypted')
