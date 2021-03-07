37ce9444cc6b4a25bb6777692331d5d05e8dd90a
Quazar8 <barcode12d@gmail.com>
2021-03-07 14:45:58 +0200

    Checking original text input for char that are letters

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,25 @@
+const keywordDOM = document.getElementById('keyword')
 const original = document.getElementById('original')
 const originalEncrypted = document.getElementById('original-encrypted')
 const scrambled = document.getElementById('scrambled')
 const decrypted = document.getElementById('decrypted')
+
+const isLetter = (c) => /[a-zA-Z]/.test(c)
+
+original.oninput = (e) => {
+    const keyword = keywordDOM.innerText
+    if (!keyword) {
+        return
+    }
+
+    const text = original.innerText
+    let result = ''
+    for (let i = 0; i < text.length; i++) {
+        let char = text[i]
+        if (isLetter(char)) {
+            console.log(char)
+        }
+    }
+
+    originalEncrypted.innerText = result
+}
