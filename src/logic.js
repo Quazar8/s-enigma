d4d73543e24828c936a93a3236cf0450a0e21fc4
Quazar8 <barcode12d@gmail.com>
2021-03-07 22:44:41 +0200

    Decrypting an encrypted messaage in the app

--- src/logic.js
+++ src/logic.js
@@ -39,3 +39,39 @@ originalDOM.oninput = (e) => {
     originalEncryptedDOM.innerText = result
 }
 
+const decrypt = (c, i) => {
+    const upper = c.toUpperCase()
+    const cCode = upper.charCodeAt(0)
+    const keyword = keywordDOM.innerText.
+        replace(/\s/g, '').toUpperCase()
+
+    const keyCode = keyword.charCodeAt(i % keyword.length)
+    let decCode = 65 + (25 + cCode - keyCode) % 25
+    if (cCode > keyCode) {
+        decCode--
+    }
+
+    return String.fromCharCode(decCode)
+}
+
+scrambledDOM.oninput = (e) => {
+    const keyword = keywordDOM.innerText
+    if (!keyword) {
+        return
+    }
+
+    const text = scrambledDOM.innerText
+    let result = ''
+    let keyCounter = 0
+    for (let i = 0; i < text.length; i++) {
+        let char = text[i]
+        if (isLetter(char)) {
+            result += decrypt(char, keyCounter)
+            keyCounter++
+        } else {
+            result += char
+        }
+    }
+
+    decryptedDOM.innerText = result
+}
