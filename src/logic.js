758a90ee9406862d8224ab70754e7fe8c81d4c8d
Quazar8 <barcode12d@gmail.com>
2021-03-07 17:32:00 +0200

    Implementing the encryption of the original text on input

--- src/logic.js
+++ src/logic.js
@@ -6,6 +6,16 @@ const decrypted = document.getElementById('decrypted')
 
 const isLetter = (c) => /[a-zA-Z]/.test(c)
 
+const encrypt = (c, i) => {
+    const upper = c.toUpperCase()
+    const cCode = upper.charCodeAt(0)
+    const keyword = keywordDOM.innerText.toUpperCase()
+
+    const keyCode = keyword.charCodeAt(i % keyword.length)
+    const encCode = 65 + (keyCode + cCode - 129) % 26 
+    return String.fromCharCode(encCode)
+}
+
 original.oninput = (e) => {
     const keyword = keywordDOM.innerText
     if (!keyword) {
@@ -14,10 +24,14 @@ original.oninput = (e) => {
 
     const text = original.innerText
     let result = ''
+    let keyCounter = 0
     for (let i = 0; i < text.length; i++) {
         let char = text[i]
         if (isLetter(char)) {
-            console.log(char)
+            result += encrypt(char, keyCounter)
+            keyCounter++
+        } else {
+            result += char
         }
     }
 
