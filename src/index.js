3512e8186a23b97bb42ff4e83df79b2f8252eda9
Quazar8 <barcode12d@gmail.com>
2021-03-08 14:23:32 +0200

    Removing the frames from the app and changing size

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 300,
+    frame: false,
   });
 
   // and load the index.html of the app.
