a28c70715daf48d7e3caeddfcf78058dd174f996
Quazar8 <barcode12d@gmail.com>
2021-03-11 20:43:02 +0200

    Method for dynamically creating triangles to the svg background

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,8 +1,24 @@
 const svg = document.getElementById('background-svg')
 const uri = 'http://www.w3.org/2000/svg'
 
-const triangleOuter = document.createElementNS(uri,'polygon')
-triangleOuter.id = 'outer-triangle'
-triangleOuter.setAttribute('points', '250,41.5 125,258.5 375,258.5')
+const appendTriangle = (sideLen, id) => {
+    const xMid = 250, yMid = 150
 
-svg.appendChild(triangleOuter)
+    const triangle = document.createElementNS(uri,'polygon')
+    triangle.id = id
+    
+    let halfLen = sideLen / 2
+    let x1 = xMid - halfLen
+    let x2 = xMid + halfLen
+
+    let vLen = Math.sqrt(sideLen * sideLen - halfLen * halfLen)
+    yUpper = yMid - vLen / 2
+    yLower = yMid + vLen / 2
+
+    const points = `${xMid},${yUpper} ${x1},${yLower} ${x2},${yLower}`
+
+    triangle.setAttribute('points', points)
+    svg.appendChild(triangle)
+}
+
+appendTriangle(250, 'outer-triangle')
